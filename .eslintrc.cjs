25aed81a303a5551cecf10c022d2dfdfcef72f50
Luotiliivi <228652844+Luotiliivi@users.noreply.github.com>
2025-10-24 09:55:19 +0300

    feat(mod-ui/github-actions): added check.yml

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,39 +7,46 @@
  */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  plugins: ["@typescript-eslint", "prettier", "import"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
   ],
   rules: {
     // Delegate formatting concerns to Prettier
-    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+    "prettier/prettier": ["warn", { endOfLine: "auto" }],
     // Warn about unused variables but ignore args prefixed with `_`
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     // Discourage usage of `any`
-    '@typescript-eslint/no-explicit-any': 'warn',
+    "@typescript-eslint/no-explicit-any": "warn",
     // Maintain a consistent import ordering
-    'import/order': [
-      'warn',
+    "import/order": [
+      "warn",
       {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        'newlines-between': 'always'
-      }
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
     ],
     // Prefer trailing commas where valid in ES5 (objects, arrays, etc.)
-    'comma-dangle': ['warn', 'always-multiline'],
+    "comma-dangle": ["warn", "always-multiline"],
     // Avoid leaving debugging code in production
-    'no-console': ['warn', { allow: ['warn', 'error'] }]
+    "no-console": ["warn", { allow: ["warn", "error"] }],
   },
-  ignorePatterns: ['dist', 'node_modules']
+  ignorePatterns: ["dist", "node_modules"],
 };
